c34528dc2d4dcd8549b01b7413c35ac18175c36c
Matthew Cassani <mrc05180@vtc.vsc.edu>
2019-04-18 12:36:10 -0400

    GameView additions and fixes.
    Fixed drawing cursor issue and added map control toolbar (partial functionality atm). Slight look/feel changes.

---
+++ DADS/DADS/Scripts/map.js
@@ -0,0 +1,85 @@
+﻿var canvasDiv = document.getElementById('mapArea');
+var canvas = document.createElement('canvas');
+var clickX = new Array();
+
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+canvas.setAttribute("height", 600);
+canvas.setAttribute("width", 800);
+canvas.setAttribute('style', "background-color:lightgray");
+canvas.setAttribute('id', "canvas");
+canvas.style.cursor = 'crosshair';
+canvasDiv.appendChild(canvas);
+if (typeof G_vmlCanvasManager != 'undefined') {
+	canvas = G_vmlCanvasManager.initElement(canvas);
+}
+ctx = canvas.getContext("2d");
+
+/* taken from http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/ */
+$('#canvas').mousedown(function (e) {
+    var mouseX = e.pageX - canvas.offsetParent.offsetLeft;
+    var mouseY = e.pageY - canvas.offsetParent.offsetTop - 72; // figure out why i need to subtract 72 from the top...
+
+    //console.log("e.pageX=%d canvas.offsetParent.offsetLeft=%d", e.pageX, canvas.offsetParent.offsetLeft)
+    //console.log("e.pageY=%d canvas.offsetParent.offsetTop - 72=%d", e.pageY, canvas.offsetParent.offsetLeft)
+
+	paint = true;
+	addClick(mouseX, mouseY);
+	redraw();
+});
+
+$('#canvas').mousemove(function (e) {
+    if (paint) {
+        addClick(e.pageX - canvas.offsetParent.offsetLeft, e.pageY - canvas.offsetParent.offsetTop - 72, true);
+		redraw();
+	}
+});
+
+$('#canvas').mouseup(function (e) {
+	paint = false;
+});
+
+$('#canvas').mouseleave(function (e) {
+	paint = false;
+});
+
+$('#eraseBtn').click(function () {
+	clickX = new Array();
+	clickY = new Array();
+	clickDrag = new Array();
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+});
+
+$('#chngMapBtn').click(function (e) {
+    
+})
+
+
+
+function addClick(x, y, dragging) {
+	clickX.push(x);
+	clickY.push(y);
+	clickDrag.push(dragging);
+}
+
+function redraw() {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	ctx.strokeStyle = "#000000";
+	ctx.lineJoin = "round";
+	ctx.lineWidth = 5;
+
+	for (var i = 0; i < clickX.length; i++) {
+		ctx.beginPath();
+		if (clickDrag[i] && i) {
+			ctx.moveTo(clickX[i - 1], clickY[i - 1]);
+		} else {
+			ctx.moveTo(clickX[i] - 1, clickY[i]);
+		}
+		ctx.lineTo(clickX[i], clickY[i]);
+		ctx.closePath();
+		ctx.stroke();
+	}
+}
